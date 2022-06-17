ba46300ff3543dbda143a169492585e17ff52f27
shay <22634623+Shay-M@users.noreply.github.com>
2022-06-17 20:40:44 +0300

    Add files via upload

---
+++ git_fix.py
@@ -0,0 +1,4 @@
+link = "https://github.com/Shay-M/RootShayMu/blob/master/Jungle%20Battle%20-%20valid%20privacy%20policy.txt"
+
+
+print(link.replace("github.com", "raw.githubusercontent.com").replace("/blob/", "/"))
